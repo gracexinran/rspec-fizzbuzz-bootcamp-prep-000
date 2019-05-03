3e8a536bb0c4e40a76ff89093d9e6d28dc8a239f
Xinran Che <gracexinran@gmail.com>
2019-05-03 00:24:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 != 0 
-    "Fizz"
+    puts "Fizz"
   elsif number % 5 == 0 && number % 3 != 0 
     "Bizz"
   elsif number %3 == 0 && number % 5 == 0 
